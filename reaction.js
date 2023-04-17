808da6af5986f67f70ff139c0f60a41932f8d567
JuanCaPriego <86634749+JuanCaPriego@users.noreply.github.com>
2023-04-16 22:32:37 -0600

    purgedcode with lists
    shows clicks and intervals in arrays

--- reaction.js
+++ reaction.js
@@ -1,69 +1,10 @@
-function getRandomColor() {
-				
-    var letters = "0123456789ABCDEF".split('');
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.round(Math.random() * 15)];
-    } //ends for loop 
-    return color; 
-
-
-} // ends getRandomColor Function
-
-
 var clickedTime; var createdTime; var reactionTime; 
 
-function makeBox() {
-        var time=Math.random();
-        time=time*3000;
-        console.log(time)
-    
-    setTimeout(function() {
-    
-        if (Math.random()>0.5) {
-        
-            document.getElementById("box").style.borderRadius="100px";
-            
-            } else {
-            
-                document.getElementById("box").style.borderRadius="0";
-            }
-            
-        var top= Math.random();
-            top= top*300;
-        var left= Math.random();
-            left= left*500; 
-            
-        document.getElementById("box").style.top = top + "px";
-        document.getElementById("box").style.left = left + "px"; 
-    
-        document.getElementById("box").style.backgroundColor=getRandomColor();
-    
-        document.getElementById("box").style.display="block";
-        
-        createdTime=Date.now();
-        
-    }, time); 
-
-}
-
-// document.getElementById("box").onclick=function() {
-
-//     clickedTime=Date.now();
-    
-//     reactionTime=(clickedTime-createdTime)/1000;
-    
-//     document.getElementById("printReactionTime").innerHTML="Your Reaction Time is: " + reactionTime + "seconds";
-    
-//     this.style.display="none";
-    
-//     makeBox();
-    
-    
-// }
 var startTime,a,b,c,d,e,f,g,h,i,j;
 var startFunc, endFunc, startFunc1, endFunc1;
 var i = 0;
+var l = [];
+var l_int = [];
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
@@ -73,6 +14,7 @@ function getReaction(){
     document.getElementById('background2').style.display='inline';
     interval = randomIntFromInterval(2,5);
     interval = interval*1000;
+    l_int.push(interval/1000);
     //console.log(interval);
     setTimeout(function(){
         startTime = Date.now();
@@ -83,6 +25,7 @@ function getReaction(){
             clickedTime = Date.now()
             
             console.log((clickedTime-startTime)/1000); 
+            l.push((clickedTime-startTime)/1000);
         }     
     },interval);
     intervalTwo = interval+1000;
@@ -103,41 +46,8 @@ function getReaction(){
 
 document.getElementById('app').ondblclick=function(){
     console.log('click');
-        // document.getElementById('background3').style.display='none';
-        // document.getElementById('background1').style.display='none';
-        // document.getElementById('background2').style.display='inline';
-        // interval = randomIntFromInterval(2,5);
-        // interval = interval*1000;
-        // console.log(interval);
-        // setTimeout(function(){
-        //     startTime = Date.now();
-        //     document.getElementById('background2').style.display='none';
-        //     document.getElementById('background1').style.display='none';
-        //     document.getElementById('background3').style.display='inline';
-        //     b = document.getElementById('background3').onclick=function(){
-        //         clickedTime = Date.now()
-                
-        //         console.log((clickedTime-startTime)/1000); 
-        //     }     
-        // },interval);
-        // intervalTwo = interval+1000;
-        // setTimeout(function(){
-        //     document.getElementById('background3').style.display='none';
-        //     document.getElementById('background2').style.display='none';
-        //     document.getElementById('background1').style.display='inline';
-        // },intervalTwo);
-        // intervalThree = intervalTwo+5000;
-        // setTimeout(function(){
-        //     document.getElementById('background3').style.display='none';
-        //     document.getElementById('background1').style.display='none';
-        //     document.getElementById('background2').style.display='inline';
-        // },intervalThree);
-        // i = i+1;
     a = getReaction();
 
-    //console.log('a', a);
-   
-    //console.log('a', a);
     setTimeout(b = function(){
         startFunc = Date.now();
         time = getReaction();
@@ -168,6 +78,8 @@ document.getElementById('app').ondblclick=function(){
                                             document.getElementById('background3').style.display='none';
                                             document.getElementById('background2').style.display='none';
                                             document.getElementById('background1').style.display='none';
+                                            console.log(l);
+                                            console.log(l_int);
                                         },k);
                                         
                                     },j);
@@ -180,22 +92,7 @@ document.getElementById('app').ondblclick=function(){
         },a+intevalFunc);
     },a);
     
-    
-    // d = getReaction();
-    // setTimeout(function(){
-    //     d = getReaction();
-    // },a+b+c+d);
-    // // e = getReaction();
-    // setTimeout(function(){
-    //     e = getReaction();
-    // },a+b+c+d+e);
 }
-
-
-
-
-//makeBox(); 
-
 
 
 
